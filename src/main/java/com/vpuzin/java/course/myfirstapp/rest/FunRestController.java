fc65c333115ab439204e13518a0435b82cd71b67
vpuzin <vpuzin@codemastersintl.com>
2023-10-13 16:38:43 +0300

    15. read application properties

--- src/main/java/com/vpuzin/java/course/myfirstapp/rest/FunRestController.java
+++ src/main/java/com/vpuzin/java/course/myfirstapp/rest/FunRestController.java
@@ -1,13 +1,19 @@
 package com.vpuzin.java.course.myfirstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${app.name}")
+    private String appName;
+    @Value("${app.author}")
+    private String appAuthor;
+
     // expose "/" return hello world
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World!";
+        return this.appName + this.appAuthor;
     }
 }
